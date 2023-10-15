3bfc7d4edbf6500945b78fbc803e9993de746ce3
alexgamer00 <alex.gamer.unity@gmail.com>
2023-10-15 13:35:50 +0200

    LeviPonti-start

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// window.addEventListener("resize", function () {
+//     // Questa funzione verrà eseguita quando la finestra del browser cambia dimensione
+//     let h = window.innerHeight;
+//     let w = window.innerWidth;
+//     let altezzaNavBar = document.querySelector('.navbar').clientHeight;
+//     cambioCarusel(w, h, altezzaNavBar);
+
+//     console.log("Nuova altezza: " + h + ", " + altezzaNavBar + " pixel");
+// });
+
+
+// function cambioCarusel(w, h, altezzaNavBar) {
+//     let carouselI = document.getElementById("carouselPricipale");
+//     carouselI.style.height = "10px";
+//     // carouselI.style.height = (h - altezzaNavBar) + "px";
+//     console.log("altezza carouselI " + carouselI.style.height);
+// }
